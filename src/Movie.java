54e9ffe7fb7529abc761d97adbf98d5e530b289d
Akash <akashpoddar492@gmail.com>
2025-08-14 13:26:44 +0530

    Initial commit: set up MovieInfoProject with Movie class

---
+++ src/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+    public static void main(String[] args){
+        String title = "RRR";
+        String director ="Raja Moli";
+        double duration = 2.56;
+
+        System.out.println("Movie Title: " + title +" ,Director: " + director + " ,Duration: "+ duration);
+    }
+}
